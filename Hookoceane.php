4b812bc2627395641ff92215b81b3823b810e766
oceanelouerat <62375391+oceanelouerat@users.noreply.github.com>
2020-03-30 11:38:35 +0200

    V2

--- Hookoceane.php
+++ Hookoceane.php
@@ -1,28 +1,4 @@
 <?php
-// Template name : Océane LOUËRAT
+// Template Name: hookoceane
 
-?> 
-
-Ceci est mon nouveau template hookocéane.php
-
-{            
-    add_action( 'wp_enqueue_scripts', 'b7ectg_theme_enqueue_styles' );
-    
-    function b7ectg_theme_enqueue_styles() {
-        wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-        wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
-    } 
-echo 'template Oceane'
-}
-
-// <?php
-// $pages = get_pages();
-// echo '<p>LISTE</p>';
-// Foreach ($pages as page) {
-// 	echo($page->post_tittle . '<br>');
-// }
-
-// echo 'DEBUG';
-// echo '<pre> ;
-// print_r($pages);
-// echo '</prez>';
+do_action("init");
